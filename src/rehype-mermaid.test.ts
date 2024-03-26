19f835f0c7f999b6d9d6ab2fbe020e0efb38e39e
Remco Haszing <remcohaszing@gmail.com>
2024-03-26 11:37:40 +0100

    Revert "Update lockfile"
    This reverts commit 529e9c27b6696d1a993e493d66c88cf7f758e00d.
    
    This commit changed how Playwright creates screenshots.

--- src/rehype-mermaid.test.ts
+++ src/rehype-mermaid.test.ts
@@ -12,7 +12,6 @@ import { VFileMessage } from 'vfile-message'
 testFixturesDirectory({
   directory: new URL('../fixtures', import.meta.url),
   prettier: true,
-  write: true,
   tests: {
     'img-png.html'(file) {
       return rehype().use(rehypeMermaid, { strategy: 'img-png' }).process(file)
